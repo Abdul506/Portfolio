6d91d824fb9e59d2781b910885d2a3464f63ad12
Abdul506 <109149932+Abdul506@users.noreply.github.com>
2024-01-22 11:13:13 -0500

    Update int_subscription_charges.sql

--- models/intermediate/int_subscription_charges.sql
+++ models/intermediate/int_subscription_charges.sql
@@ -3,7 +3,7 @@ with revenuecat as (
     select distinct
         `ORIGINAL_TRANSACTION_ID` as event_id, 
         transaction_id as subscription_id,
-        --`ID` as event_id, 
+        `ID` as event_id, 
         `USER_ID` as customer_id, 
         max(case when event_name in ('rc_renewal_event', 'rc_initial_purchase_event') then timestamp end) over (partition by transaction_id) as subscription_created_at,
         max(case when event_name in ('rc_cancellation_event') then timestamp end) over (partition by transaction_id) as SUBSCRIPTION_CANCELLED_AT,
